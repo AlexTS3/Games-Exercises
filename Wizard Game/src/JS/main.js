d36363193895e83db445a1222aab0b1f064e0758
AlexTS3 <tsvetanovalex@yahoo.com>
2022-12-22 22:20:23 +0200

    Added an other version of code

---
+++ Wizard Game/src/JS/main.js
@@ -0,0 +1,28 @@
+let state = initState();
+let game = initGameObjects();
+const avaliableKeys = [
+    'KeyA',
+    'KeyS',
+    'KeyD',
+    'KeyW'
+]
+document.addEventListener('keydown', (e) => {
+    if(avaliableKeys.includes(e.code)){
+        state.keys[e.code] = true;
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    if(avaliableKeys.includes(e.code)){
+        state.keys[e.code] = false;
+    }
+})
+
+game.startScreen.addEventListener('click', (e) => {
+
+    game.startScreen.classList.add('hidden');
+    game.gameScreen.classList.remove('hidden');
+
+    start(state, game)
+});
+
