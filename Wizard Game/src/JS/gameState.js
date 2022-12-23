d05d442d0dc03a4113939176fe1ecd1063ec56e7
AlexTS3 <tsvetanovalex@yahoo.com>
2022-12-23 19:13:14 +0200

    Add bug spawn delay

--- Wizard Game/src/JS/gameState.js
+++ Wizard Game/src/JS/gameState.js
@@ -14,7 +14,9 @@ function initState(){
 
         bugStats: {
             width: 50,
-            height: 50
+            height: 50,
+            nextSpawnTimestamp: 0,
+            maxSpawnInterval: 1500
         },
         keys: {
             KeyA: false,
